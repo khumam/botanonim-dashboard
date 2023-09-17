f2dbcb875abb59d1a9630bdd182ef5bd5d8990e3
Khoerul Umam <khoerul.umam@gotoko.co.id>
2023-09-17 12:54:19 +0700

    up: filter

--- app/Http/Livewire/MessageUserLivewire.php
+++ app/Http/Livewire/MessageUserLivewire.php
@@ -3,16 +3,24 @@
 namespace App\Http\Livewire;
 
 use App\Models\Message;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class MessageUserLivewire extends Component
 {
     public $chatId;
     public $messages = [];
+    public $date = '';
 
     public function mount()
     {
-        $this->messages = Message::where('chat_id', $this->chatId)->orderBy('date', 'desc')->get();
+        $this->date = Carbon::parse(Carbon::now())->format('Y-m-d');
+        $this->filter();
+    }
+
+    public function filter()
+    {
+        $this->messages = Message::where('chat_id', $this->chatId)->whereDate('date', $this->date)->orderBy('date', 'desc')->get();
     }
 
     public function render()
--- resources/views/livewire/message-user-livewire.blade.php
+++ resources/views/livewire/message-user-livewire.blade.php
@@ -1,10 +1,13 @@
 <div class="card">
-    <div class="card-header">
+    <div class="card-header d-flex align-items-center">
 			<h4 class="card-title">Pesan terkirim oleh terlapor</h4>
+			<div>
+				<input type="date" class="form-control" wire:model="date" wire:change="filter">
+			</div>
 		</div>
 		<div class="card-body">
 			@forelse($messages as $message)
-			<p>- {{ $message->text }}</p>
+			<p>[{{ $message->date }}]: {{ $message->text }}</p>
 			@empty
 			Tidak ada pesan
 			@endforelse
